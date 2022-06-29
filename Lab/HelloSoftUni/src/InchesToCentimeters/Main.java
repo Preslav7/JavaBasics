73824101c8abd3c50d7f7728c46ca4d1dc8f2b8f
Preslav Ivanov <preslavivanov7@gmail.com>
2022-06-29 18:54:25 +0300

    Create Main.java

---
+++ Lab/HelloSoftUni/src/InchesToCentimeters/Main.java
@@ -0,0 +1,14 @@
+package InchesToCentimeters;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double inches =Double.parseDouble(scanner.nextLine());
+        double centimeters = inches * 2.54;
+
+        System.out.println(centimeters);
+    }
+}
