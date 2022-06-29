28be8327f99b656356215d2d026b9b4dde34c747
Preslav Ivanov <preslavivanov7@gmail.com>
2022-06-29 18:51:35 +0300

    Main

---
+++ Lab/HelloSoftUni/src/RectangleAngle/Main.java
@@ -0,0 +1,14 @@
+package RectangleAngle;
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+        int area = a * b;
+
+        System.out.println(area);
+    }
+}
